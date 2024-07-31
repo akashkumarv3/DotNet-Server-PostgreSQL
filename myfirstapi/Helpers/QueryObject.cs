67b8c90d735dea1a4cf97299d69452f54e77c52d
akashkumarv3 <akashkumarv@boston-technology.com>
2024-07-31 13:22:07 +0530

    Sorting by field name for stocks model

--- myfirstapi/Helpers/QueryObject.cs
+++ myfirstapi/Helpers/QueryObject.cs
@@ -7,8 +7,11 @@ namespace myfirstapi.Helpers
 {
     public class QueryObject
     {
-        public String? Symbol { get; set; }=null;
+        public String? Symbol { get; set; } = null;
 
-        public String? CompanyName { get; set; }=null;
+        public String? CompanyName { get; set; } = null;
+        public String? SortBy { get; set; } = null;
+
+        public bool IsDescending { get; set; } = false;
     }
 }
--- myfirstapi/Repository/StockRepository.cs
+++ myfirstapi/Repository/StockRepository.cs
@@ -31,30 +31,39 @@ namespace myfirstapi.Repository
 
         public async Task<Stock?> DeleteAsync(int id)
         {
-           var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
-                  if(stockModel==null){
-                      return null;
-                  }
-               
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return null;
+            }
 
-              //now delete the stockModel from db
-               _context.Remove(stockModel);
-              await _context.SaveChangesAsync();
-                return stockModel;
+
+            //now delete the stockModel from db
+            _context.Remove(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
         }
 
         public async Task<List<Stock>> GetAllAsync(QueryObject query)
         {
-            var stocks=  _context.Stocks.Include(c=>c.Comments).AsQueryable();
-            
+            var stocks = _context.Stocks.Include(c => c.Comments).AsQueryable();
 
-            if(!string.IsNullOrWhiteSpace(query.CompanyName ))
+
+            if (!string.IsNullOrWhiteSpace(query.CompanyName))
             {
-                 stocks=stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
+                stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
             }
-             if(!string.IsNullOrWhiteSpace(query.Symbol ))
+            if (!string.IsNullOrWhiteSpace(query.Symbol))
             {
-                 stocks=stocks.Where(s => s.Symbol.Contains(query.Symbol));
+                stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
+            }
+
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
             }
 
             return await stocks.ToListAsync();
@@ -62,36 +71,38 @@ namespace myfirstapi.Repository
 
         public async Task<Stock?> getByIdAsync(int id)
         {
-            var stock=await _context.Stocks.Include(c=>c.Comments).FirstOrDefaultAsync(i=>i.Id==id);
+            var stock = await _context.Stocks.Include(c => c.Comments).FirstOrDefaultAsync(i => i.Id == id);
 
-          if(stock==null){
-            return null;
-          }
-          return stock;
+            if (stock == null)
+            {
+                return null;
+            }
+            return stock;
         }
 
         public async Task<bool> StockExists(int? stockId)
         {
-            return  await _context.Stocks.AnyAsync(x=>x.Id==stockId);
+            return await _context.Stocks.AnyAsync(x => x.Id == stockId);
         }
 
         public async Task<Stock?> UpdateAsync(int id, UpdateStocRequestDto updateStocDto)
         {
-            var stockModel =await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
-                  if(stockModel==null){
-                      return null;
-                  }
-               stockModel.Symbol=updateStocDto.Symbol;
-               stockModel.CompanyName=updateStocDto.CompanyName;
-               stockModel.Purchase=updateStocDto.Purchase;
-               stockModel.Indunstry=updateStocDto.Indunstry;
-               stockModel.LastDiv=updateStocDto.LastDiv;
-               stockModel.MarketCap=updateStocDto.MarketCap;
+            var stockModel = await _context.Stocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return null;
+            }
+            stockModel.Symbol = updateStocDto.Symbol;
+            stockModel.CompanyName = updateStocDto.CompanyName;
+            stockModel.Purchase = updateStocDto.Purchase;
+            stockModel.Indunstry = updateStocDto.Indunstry;
+            stockModel.LastDiv = updateStocDto.LastDiv;
+            stockModel.MarketCap = updateStocDto.MarketCap;
 
-              //now save the stockModel chnages to db
-             await _context.SaveChangesAsync();
+            //now save the stockModel chnages to db
+            await _context.SaveChangesAsync();
 
-             return stockModel;
+            return stockModel;
         }
     }
 }
